9d7e7a5d8c7eeac31da4de2c8036c530c97a7ff4
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-05 17:53:57 +0100

    Commit 8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
+    $('#invia').click(function(){
+        var messaggioInput = $('#insert-message').val();
+        $('#insert-message').val('');
+        var messaggio = $('.template .message-sent-1').clone();
+        messaggio.children('.text-message').text(messaggioInput);
+        messaggio.children('.timetable').text('16:40');
+
+        //Inseriamo nell'html il messaggio
+        $('.messages-sent').append(messaggio);
 
 
 
-    
+
+    });
+
+
+
+
 });
