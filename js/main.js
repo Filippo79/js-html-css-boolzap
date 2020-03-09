1ea496f2f011d7713518725115e2894a485a0fd6
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-09 09:27:12 +0100

    Commit 11

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
-    $('#invia').click(function(){
-        var messaggioInput = $('#insert-message').val();
-        $('#insert-message').val('');
-        var messaggio = $('.template .message-sent-1').clone();
-        messaggio.children('.text-message').text(messaggioInput);
-        messaggio.children('.timetable').text('16:40');
+        $('.icons-text-right i').click(function(){
+            var messaggioInput = $('.insert-message').val();
+            if (messaggioInput.trim().length > 0) {
+            $('.insert-message').val('');
+            var messaggio = $('.template .message-sent-1').clone();
+            messaggio.children('.text-message').text(messaggioInput);
+            messaggio.children('.timetable').text('16:40');
+            $('.messages-sent').append(messaggio);
 
-        $('.messages-sent').append(messaggio);
-    });
 
-    $('#search-contact').keiup(function(event){
-        var variabileFiltroCarattere = $(this).val().toLowerCase();
-        $('#contact').each(function(){
-            if ($(this).text().toLoveCase().includes(variabileFiltroCarattere)){
-                $(this).shov();
-            }else {
-                $(this).hide();
+            setTimeout(function() {
+                var creaMessaggio = $('.template .message-received-1').clone();
+                creaMessaggio.children('.text-message').text('ok');
+                creaMessaggio.children('.timetable').text('16:40');
+                $('.messages-received').append(creaMessaggio);
+
+            }, 1000);
+
             }
         });
+
+
+
+
+        function scroll() {
+          var pixelScroll = $('.message-sent-1').height();
+          $('.message-sent-1').scrollTop(pixelScroll);
+     }
+
+
+
+
+
     });
-});
