8447715a9b9df4b18def1cf6b5ba9fdd57283e29
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-12 17:54:25 +0100

    Commit 14

--- js/main.js
+++ js/main.js
@@ -3,81 +3,71 @@ $(document).ready(function() {
         $('.icons-text-right i').toggleClass('fas fa-microphone fas fa-paper-plane');
     }).blur(function(){
     });
-        $('.icons-text-right i').click(function(){
-            var messaggioInput = $('.insert-message').val();
-            if (messaggioInput.trim().length > 0) {
-                $('.insert-message').val('');
-                var messaggio = $('.template .message-sent-1').clone();
-                messaggio.children('.text-message').text(messaggioInput);
-                messaggio.children('.timetable').text('16:40');
-                $('.message.active').append(messaggio);
 
+    $('.icons-text-right i').click(inviaMessaggio);
+    $('.search-contact').keyup(ricercaContatto);
 
-                setTimeout(function() {
-                    var creaMessaggio = $('.template .message-received-1').clone();
-                    creaMessaggio.children('.text-message').text('ok');
-                    creaMessaggio.children('.timetable').text('16:40');
-                    $('.message.active').append(creaMessaggio);
-
-                }, 1000);
+    $('.single-contact').click(function(){
+        var nome = $(this).text();
+        //console.log(nome);
+        var contatto =$(this).data('numContact');
+        //console.log(contatto);
+        $('.message').each(function(){
+            if(contatto == $(this).data('numChat')){
+                $('.message').hide();
+                $(this).show().toggleClass('active');
             }
         });
-        $('.search-contact').keyup(function(event){
-            var ricercaContatto = $(this).val().toLowerCase();
-            //console.log(ricercaContatto);
-            $('.single-contact').each(function() {
-                var contatto = $(this).find('.name h4').text().toLowerCase();
-                if(contatto.includes(ricercaContatto)) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
+    })
+});
 
-        });
-        //Secondo metodo ricerca in contatti
-        /*$('.search-contact').keyup(function(event){
-            var ricercaContatto = $(this).val().toLowerCase();
-            //console.log(ricercaContatto);
-            $('.name h4').each(function() {
-                 if($(this).text().toLowerCase().includes(ricercaContatto)){
-                     $(this).parentsUntil('.contacts').show();
-                }else {
-                     $(this).parentsUntil('.contacts').hide();
-                }
-            });
-        });*/
+//Funzione che permette di scrollare automaticamente la chat
+function scroll() {
+    var messagesScroll = $('.message').height();
+    $('.message').scrollTop(messagesScroll);
+}
 
-        $('.single-contact').click(function(){
-            var nome = $(this).text();
-            //console.log(nome);
-            var contatto =$(this).data('numContact');
-            //console.log(contatto);
-                $('.message').each(function(){
-                    if(contatto == $(this).data('numChat')){
+//Evento che crea il messaggio che si è scritto
+function inviaMessaggio(){
+    var messaggioInput = $('.insert-message').val();
+    if (messaggioInput.trim().length > 0) {
+        $('.insert-message').val('');
+        /*var messaggio = $('.template .message-sent-1').clone();
+        messaggio.children('.text-message').text(messaggioInput);
+        messaggio.children('.timetable').text('16:40');
+        $('.message.active').append(messaggio);*/
+        //-----------------------Handlebars------------------------------
+        var source = $('#message-template-sent').html();// clono il template messaggio
+        var template = Handlebars.compile(source); // do in pasto ad Handlebars il template clonato
+        var datiMessaggio = {
+            testoMessaggio: messaggioInput,
+            orario: '13:00'
+        };
 
-                        $('.message').hide().toggleClass('active');
-                        $(this).show();
+        var templateMessaggio = template(datiMessaggio);
+        $('.message.active').append(templateMessaggio);
 
-                    }
-                });
-        })
+//-----------------------Fine Handlebars--------------------------------------
+        setTimeout(function() {
+            var creaMessaggio = $('.entry-template .message-received-1').clone();
+            creaMessaggio.children('.text-message').text('ok');
+            creaMessaggio.children('.timetable').text('16:40');
+            $('.message.active').append(creaMessaggio);
 
+        }, 1000);
+    }
+}
 
-
-
-
-
-
-
-
-        function scroll() {
-          var messagesScroll = $('.message').height();
-          $('.message').scrollTop(messagesScroll);
-     }
-
-
-
-
-
+function ricercaContatto(event){
+    var ricercaContatto = $(this).val().toLowerCase();
+    //console.log(ricercaContatto);
+    $('.single-contact').each(function() {
+        var contatto = $(this).find('.name h4').text().toLowerCase();
+        if(contatto.includes(ricercaContatto)) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
     });
+
+}
